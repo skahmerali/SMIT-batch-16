e8c1212cc71f846c6dd7fdffeb62c8ca904d9328
ayanhussain81 <ayanhussain746@gmail.com>
2025-05-16 12:49:36 +0500

    javascript first class

---
+++ javascript/class-1/app.js
@@ -0,0 +1,18 @@
+// console.log("Hello world!");
+// document.write('Hello world!');
+// alert("Hello world");
+
+// var const let
+
+// console.log(box1);
+// var box1 = "class batch 16";
+// var box1 = "class SMIT batch-16";
+// console.log(box1);
+
+// const box1 = "class batch 16";
+// console.log(box1);
+// // box1 = "class SMIT batch-16";
+// const box1 = "class SMIT batch-16";
+// console.log(box1);
+
+// let box1 = "class batch 16";
