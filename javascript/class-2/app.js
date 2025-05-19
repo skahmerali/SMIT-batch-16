e08d36b48e3aa7049596c9a4bcace9155644c9ba
ayanhussain81 <ayanhussain746@gmail.com>
2025-05-19 12:51:35 +0500

    class 2 of js

---
+++ javascript/class-2/app.js
@@ -0,0 +1,28 @@
+// var fNumber = +prompt("Enter your first desire Number:");
+// var fName = prompt("Enter your first Name : ");
+// var sName = prompt("Enter your Second Name : ");
+// var sNumber = Number(prompt("Enter your second desire Number:"));
+
+// console.log(fNumber + sNumber);
+// console.log(fName + sName);
+
+// Data Types
+// var uservalue = "";
+// console.log(uservalue);
+// var uservalue = [];
+// var uservalue = undefined ;
+// console.log(uservalue);
+// var uservalue = null ;
+
+// var user name
+// var %username
+// var 232username
+
+
+// var $asmmnds
+// var _asdasdasd
+// var .
+
+// v
+
+
