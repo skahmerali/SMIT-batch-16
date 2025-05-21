01082c955e3d625822f6040a68b62a25b1fbffb4
skahmerali <sheikhahmerali75@gail.com>
2025-05-21 12:51:56 +0500

    increament decreament and conditional statement

---
+++ javascript/class-3/app.js
@@ -0,0 +1,64 @@
+// var name;
+
+// var hasOwn = false;
+// var isFound = false;
+// name = "ahmer";
+// var var ;
+// var rose = "gulabo";
+// alert(Rose);
+
+// var num = 3;
+// num = num + 1 === num++
+// num++; //post increament
+// ++num; //pre increament
+// console.log(num);
+
+// console.log('line num 15 ===>', ++num); //2
+// console.log('line num 16 ===>', num++); //4
+// console.log('line num 17 ===>', num); //5
+// --num; //2
+// num++; //3
+// ++num; //4
+// num++; //5
+// num--; //4
+// console.log(num++); //5
+// console.log(--num, num++, ++num, num++, num--, num++);
+// 2 2 4 4 5 4
+// console.log(num);
+//5
+// 2, 2||4, 4 || 3 , 4||3, 5||4||2, 4||5
+// console.log(num); //5
+
+
+// BODDMAS (Divide, Multiply, Add, Sub)
+
+// var num = 54;
+// num = num / 4 ;
+// num = (((num * 5) + 1) - ((num / 4) + 8));
+
+// 270 + 1 - 13.5 + 8
+// 271 - 5.5
+//  265.5
+// console.log(num);
+num = 20;
+// if (num < 20) {
+//     // console.log("number is greater than 20")
+//     console.log("number is less than 20")
+// }
+// if (num < 20) {
+//     console.log("number is less than 20");
+// } else {
+//     console.log("number is greater than 20")
+// }
+// if (num <= 20) {
+//     console.log("number is arround 20");
+// } else {
+//     console.log("number is greater than 20")
+// }
+if (num <= 20) {
+    console.log("number is arround 20");
+} else if (num >= 20) {
+    console.log("number is greater than 20")
+}else{
+    console.log("number is not arround 20")
+}
