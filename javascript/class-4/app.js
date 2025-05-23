1745ee7de1bb0fc64182354164fc5f3e5d2bacb1
skahmerali <sheikhahmerali75@gail.com>
2025-05-23 12:41:39 +0500

    opperators and opprends

---
+++ javascript/class-4/app.js
@@ -0,0 +1,65 @@
+// var num1 = 2;
+// if (num1 == '22') {
+//     alert("you are right!!!");
+// };
+// if (num1 === '22') {
+//     alert("you are right!!!");
+// };
+
+
+// var que1 = prompt("are you a student?");
+// var que2 = prompt("are you a teacher?");
+// var que3 = prompt("are you a doctor?");
+// var reward = 0;
+// if (que1 === "yes" && que2 === 'yes') {
+//     alert("You got 5 numbers!!!");
+//     reward = 15;
+// }
+// if (que2 === 'yes'){
+//      alert("You got 10 numbers!!!");
+//     reward = 10;
+// }
+
+// if (que1 == "yes") {
+//     alert("You got 5 numbers!!!");
+//     reward = 5;
+// }
+// if (que2 === "yes") {
+//     alert("You got more 10 numbers!!!");
+//     reward = reward + 10;
+// }
+// if (que3 === "yes") {
+//     alert("You got more 15 numbers!!!");
+//     reward = reward + 15;
+// }
+
+// console.log('here is your reward', reward);
+// alert('here is your reward : '+ reward);
+// document.write('here is your reward', reward);
+
+// var email = "sheikahmerali@gmail.com";
+// var password = "ahmer12345";
+// var userEmail = prompt("Enter your email");
+// var userPass = prompt("Enter your password");
+// if( email === userEmail && userPass === password){
+//  alert("login sucessfully!")
+// }else{
+//     alert("Sorry! login failed...")
+// }
+
+// &&
+// ||
+// !
+// var userEmail = "hello world";
+var userEmail;
+if (!userEmail) {
+    alert("you are right")
+}
+// !=
+// if(5 != "5"){
+
+// }
+// !==
+// ===
+// ==
+// =
