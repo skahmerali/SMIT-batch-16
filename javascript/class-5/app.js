77824f9939bbd071f1dd5a5f003bfcae555a53a3
Ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-05-26 13:02:49 +0500

    arryas and loop

---
+++ javascript/class-5/app.js
@@ -0,0 +1,36 @@
+// let userName = prompt("Enter your first name!")
+// // if (userName === "ahmer") {
+// //     if (edu) {
+// //         if (edu === "master") {
+// //              alert("welcome to the helloWorld communinty")
+// //         }
+// //     }
+// // }
+// // if (userName === "ahmer" && edu && edu === "master") {
+
+// // }
+
+// if (c === d) {
+//     if (x === y) {
+//         g = h;
+//     }
+//     else if (a === b) {
+//         g = h;
+//     }
+//     else {
+//         e = f;
+//     }
+// } else {
+//     e = f;
+// }
+
+var userName = prompt("Enter your first name!")
+// var arrays = [1, "ahmer", false, 0.2, [2, 3, 4, 6, 5, 7, 8], userName];
+
+// console.log(arrays[4][2]);
+// console.log(arrays.length);
+
+for (var i = 0; i < arrays.length; i++) {
+    // console.log(arrays[i])
+    // console.log([i])
+}
