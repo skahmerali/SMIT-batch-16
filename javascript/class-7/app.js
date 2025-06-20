d4cb903cb3b02e6c81a48de5fccc2a7b4acb0a35
skahmerali <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-06-20 12:46:24 +0500

    arrays method and brain stroming question

---
+++ javascript/class-7/app.js
@@ -0,0 +1,38 @@
+// var arrays = [];
+
+// arrays.unshift("ahmer", 2);
+
+// console.log(arrays);
+
+// arrays.shift();
+
+// console.log(arrays);
+
+// var products = [
+//   "bat",
+//   "ball",
+//   "football",
+//   "tennis-racket",
+//   "hockey",
+//   "betmintor",
+// ];
+
+// var newArrays = products.slice(0, products.length);
+// console.log(products.length-1);
+
+// sports.splice(3, 0, "volly ball");
+// console.log(sports);
+// console.log(newArrays);
+// console.log(products);
+
+// var num1 = 5;
+// var num2 = 6;
+var num1 = +prompt("Enter your first number :");
+var num2 = +prompt("Enter your second number :");
+
+// [num1, num2] = [num2, num1];
+// num1 = num1 + num2 ;
+// num2 = num1 - num2;
+// num1 = num1 - num2;
+console.log("this is your first value :", num2);
+console.log("this is your second value :", num1);
