c2e2e22b0af8ced7faba0967a2478416d211fffe
skahmerali <sheikhahmerali75@gmail.com>
2025-06-27 12:51:41 +0500

    erd

---
+++ javascript/class-10/app.js
@@ -0,0 +1,26 @@
+var companies = [
+  {
+    "compnay Name": "Bata",
+    products: [
+        { 
+            productCategory: "man",
+            varrieties : [
+                {
+                    name : 'sandils',
+                    isAvailable: 'false',
+                },
+                {
+                    name: "shoes"
+                }, 
+                {
+                    name: "slippers"
+                }, 
+            ]
+         }
+    ],
+  },
+];
+
+
+// create a complete relation ship Gient Compnay array of object
+// atleast gient company has 5 child compnaies , 
