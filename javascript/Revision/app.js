8ed9b77a8cdc547afd7ddff1dfcd49a3f62082e7
skahmerali <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-04 12:51:10 +0500

    javascript revision

---
+++ javascript/Revision/app.js
@@ -0,0 +1,69 @@
+// var let const
+// {
+//     let fname = "ahmer";
+// };
+
+//         const stdName = "ahmer";
+// // console.log(name, 7);
+// // console.log(fname, 8);
+
+// function abc () {
+//     var name = "ali";
+//     let fname = "ali";
+//     console.log(name, 15);
+//     console.log(fname, 16);
+// }
+// abc();
+
+// var stdName = "ali"
+
+// getting from user
+// var name = prompt();
+// // getting input but as a number from user
+// var number = +prompt();
+
+//  render the value and string
+// alert("ahmer")
+// alert("ahmer")
+
+// let email ;
+// if (email) {
+//   console.log("email is exist!!!");
+//   if (email === "ahmer@gmail.co") {
+//     console.log("email is correct!");
+//   }
+// } else {
+//      console.log("email is wrong!");
+// }
+
+// if (email && email === "ahmer@gmail.co") {
+// //   console.log("email is exist!!!");
+//   console.log("email is correct!");
+// } else {
+//   console.log("email is wrong!");
+// }
+
+// if (email) {
+//   console.log("email is exist!!!");
+//   if (email === "ahmer@gmail.co") {
+//     console.log("email is correct!");
+//   } else if ("ahmer@gmail.com") {
+//     console.log("email is correct!");
+//   }
+// } else {
+//   console.log("email is wrong!");
+// }
+
+// var arrays = ["ahmer", "ali", 123, true, false, ["sheikh"]];
+
+// console.log(arrays[0])
+// console.log(arrays[1])
+// console.log(arrays[2])
+// console.log(arrays[3])
+// console.log(arrays[4])
+// console.log(arrays[5])
+// console.log(arrays.length)
+
+// for (var i = 0; i < arrays.length; i++) {
+//   console.log(arrays[i]);
+// }
