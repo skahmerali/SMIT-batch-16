79fee45a3b4284a308471a7b6bba7c1cb50e2d29
skahmerali <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-14 12:57:51 +0500

    assignment discussion

--- javascript/class-13/app.js
+++ javascript/class-13/app.js
@@ -8,4 +8,46 @@
 // let userPreferences = JSON.parse(localStorage.getItem('userPreferences'));
 // console.log(userPreferences.theme)
 
+// var stdRecord = [];
+// if (!database) {
+// } else {
+//   for (var i = 0; i <= database.length; i++) {
+//     if (database[i].email === userEmail) {
+//       let result = document.getElementById("result").innerText;
+//       result = "email already exist";
+//       return result;
+//     }
+//   }
+// }
+
+// now you create a function responsible for signup user
+// create an object (signup details) push inside the array
+// first get a localstorage value  and check weather user email is exist or not
+// if email exist so show user already exist
+// else save that array inside the localstorage
+
+// now you create a function responsible for login user
+// get a locastorage item and save in the varriable
+// implement the loop and find the user email
+// if user exist so check the password
+// if passowrd and email match so show the alert of sucessfully message
+// then show the dashboard / profile / home through window.location.href = "./about.html"
+
+// homepage/ dashboard/ profile
+
+// create a function responsible for shows the blog
+// get the blogs from localstorage
+// show the create blog button
+// create an empty array (blogsDatabase)
+// create a function where add bloogs object
+// {
+//  name: '',
+//  title: '',
+// blogBody : '', 
+//  blogImg : '', (optional)
+// date: '', 
+// author : '',
+// category : '',
+// }
+// then push this obj in the array and save it inside the localstorage 
 
