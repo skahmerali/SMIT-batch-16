052aac19af196ed15cf2957b15934a18ea6ede70
skahmerali <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-23 12:44:10 +0500

    updated code

---
+++ javascript/class-15/app.js
@@ -0,0 +1,27 @@
+// var text = "sheikh ahmer ali!";
+// var text = document.getElementById("paragraph").innerText;
+
+// function findAndReplace (){
+// var find = document.getElementById("find").value;
+// var replace = document.getElementById("replace").value;
+
+// console.log(text.charAt(5));
+// console.log(text.charAt(text.length -1));
+
+// for (var i = 0; i < text.length; i++) {
+//   if (text.charAt(i) === "!") {
+//     alert("here we go : " + text.charAt(i) + " at position : " + i);
+//   }
+// }
+// }
+// var scoreAvg = 2.9;
+// // var numberOfStars = Math.round(scoreAvg);
+// // var numberOfStars = Math.ceil(scoreAvg);
+// var numberOfStars = Math.floor(scoreAvg);
+// console.log(numberOfStars)
+
+// var newNumber = Math.random();
+// console.log(newNumber * 100);
+// var hundredsNumber = newNumber * 1000;
+// console.log(Math.round(newNumber));
+// console.log(Math.round(hundredsNumber));
---
+++ javascript/class-16/app.js
@@ -0,0 +1,57 @@
+// var age = Number(prompt("enter your real age!!!"));
+// var age = parseInt(prompt("enter your real age!!!"));
+// var age = +prompt("enter your real age!!!");
+// var age = prompt("enter your real age!!!");
+
+// console.log(age + 5);
+// var profit = "200" - "150";
+// var profit = "200" - "duck";
+// console.log(profit);
+
+// const numberFloat = parseFloat("4");
+// console.log(numberFloat);
+// console.log(numberFloat.toString());
+
+// const num = 5.678;
+// console.log(num.toFixed());
+
+// const num = 12345;
+// // "12345"
+// var str = num.toString();
+// if (str.charAt(str.length - 1) === "5") {
+//   str = str.slice(0, str.length) + "6";
+//   console.log(str)
+// }
+// num = Number(str);
+// prettyNum = num.toFixed(2);
+
+// quiz practice
+
+// alert("Welcome to the pakistan", "byebye china");
+// alert("Welcome to the pakistan" + "byebye china");
+
+// illegal
+// var num first 
+// var 2323sadsa
+// var %%^
+// var bshdbsh%^
+// var asda-sdsdas
+// var var
+
+
+
+// var $sds
+// var _asd
+// var sdnjd78678
+// habib 2,2 
+// mohsin 3,2
+// umer 2,3
+// safdar 1, 2
+var num = 1;
+num++ //2
+num-- //1
+--num //0
+++num //1
+++num //2
+console.log(num--) //2, value=1
+console.log(++num) //2
