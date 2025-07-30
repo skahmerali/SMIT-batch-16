2b22af8b4493bedc927edc205c8e2e252dca2c31
skahmerali <38328235+skahmerali@users.noreply.github.com>
2025-07-30 13:09:59 +0500

    update on arrow function vs normal function

---
+++ javascript/class-17/app.js
@@ -0,0 +1,19 @@
+// var date = new Date();
+
+// console.log(date);
+
+// console.log(date.getDay());
+// console.log(date.getTime());
+
+// var fromDate = new Date('January 1, 2021');
+
+// console.log(fromDate);
+// var toDate = new Date('July 25, 2025');
+// console.log(fromDate - toDate)
+// console.log(fromDate.getTime());
+
+// var newdate = new Date();
+
+// console.log(newdate.setFullYear(2002)/(1000 * 60 * 60 * 24));
+
+
---
+++ javascript/class-18/app.js
@@ -0,0 +1,36 @@
+// function add(e) {
+//     console.log(e);
+//     return e;
+// //   var name = "ahmer";
+// //   return name;
+// }
+// const add = () => {
+
+// };
+
+// add("ahmer");
+// console.log(add('ahmer'));
+// console.log(e);
+// add(111);
+// console.log(add(false));
+
+// const multiply = (num1, num2) => num1 * num2;
+
+// function print() {
+//   console.log(arguments);
+// }
+
+// print("hello", 400, false);
+
+// {
+//   '0': 'hello',
+//   '1': 400,
+//   '2': false
+// }
+
+// const print = () => {
+//   console.log(arguments);
+// };
+
+// print("hello", 400, false);
+// Uncaught ReferenceError: arguments is not defined
