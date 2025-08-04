14a8f09e0c3bb23058de349d2df2ebee657622e2
JaffarAman <mohammadsaadali786.92@gmail.com>
2025-08-04 13:02:20 +0500

    login and signup

--- javascript/class-19/app.js
+++ javascript/class-19/app.js
@@ -1,6 +1,7 @@
 var students = [];
 
 function signup() {
+  //repeatedAction();
   var firstName = document.getElementById("fName").value;
   var lastName = document.getElementById("lName").value;
   var age = document.getElementById("age").value;
@@ -8,8 +9,8 @@ function signup() {
   var password = document.getElementById("password").value;
 
   var stdData = {
-    firstName: firstName,
-    "last Name": lastName,
+    firstName: firstName, // if the key and value are same so no need to reuse with property declartion 
+    lastName: lastName, //if we use spaces in the name so implement "" or ''
     age,
     email,
     password,
@@ -17,4 +18,24 @@ function signup() {
 
   students.push(stdData);
   window.localStorage.setItem("data", JSON.stringify(students));
+  window.location.href = "login.html"
 }
+
+const login = () => {
+  var loginEmail = document.getElementById('loginEmail').value;
+  var loginPassword = document.getElementById('loginPassword').value;
+  var getData = window.localStorage.getItem('data');
+
+  getData = JSON.parse(getData);
+  // console.log(getData[0].firstName);
+  for (var i = 0; i <= getData.length; i++ ){
+    // console.log(getData[i]);
+    if(getData[i].email === loginEmail && getData[i].password === loginPassword ){
+      console.log('hello irtiza!!!!');
+    }
+  }
+}
+
+
+
+
