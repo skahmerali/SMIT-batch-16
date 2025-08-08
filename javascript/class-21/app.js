09e09cdbbd2197a5e02531baac6f0ce9548a61c3
HasanCoder66 <mohammadsaadali786.92@gmail.com>
2025-08-08 12:54:47 +0500

    DOM

---
+++ javascript/class-21/app.js
@@ -0,0 +1,10 @@
+// var div = document.getElementById("paragraphes");
+// console.log(div, "line number 2");
+// // var p = div[1];
+// // console.log(p, 'line number 4');
+
+// var p = div.getElementsByTagName('p');
+// console.log(p[1].innerHTML, 'line number 7')
+
+var body = document.childNodes[1].childNodes[2].childNodes[1].childNodes[1].childNodes[0];
+console.log(body);
