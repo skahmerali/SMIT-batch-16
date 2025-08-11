1a2f4d41dbcf5b1a1e5c04b2a571953ba3e262f9
HasanCoder66 <mohammadsaadali786.92@gmail.com>
2025-08-11 12:57:59 +0500

    DOM

---
+++ javascript/class-22/app.js
@@ -0,0 +1,10 @@
+// var body = document.childNodes[1].childNodes[2].childNodes
+// console.log(body);
+
+var div = document.getElementById('div');
+// console.log(div);
+// var textDiv = div.childNodes[1].childNodes[0];
+// console.log(textDiv.nodeType);
+var textDiv2ndP = div.childNodes[2].childNodes[0];
+console.log(textDiv2ndP.nodeType);
+console.log(textDiv2ndP.nodeValue);
