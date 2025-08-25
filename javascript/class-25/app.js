c15a5b5cf4db63889bcaa1cfb9ed5bb13d21e8b4
HasanCoder66 <mohammadsaadali786.92@gmail.com>
2025-08-25 12:56:00 +0500

    events handler

---
+++ javascript/class-25/app.js
@@ -0,0 +1,23 @@
+// function zipCodeChecker() {
+
+
+//     let zipCode = document.getElementById('zipcode').value;
+//     switch (zipCode) {
+//         case '05444':
+//             areaName = 'sadar';
+//             break;
+//         case '16250':
+//             areaName = 'DHA';
+//             break;
+//         default:
+//             areaName = 'karachi';
+
+//     };
+//     document.getElementById('city').value= areaName;
+
+
+// }
+
+function picVisibility (){
+    document.getElementById('invisible').className = 'hidden';
+}
