f6a1983552747395c16663aa52a1d4a32b12725c
Talal <ta234567801@gmail.com>
2025-09-17 13:11:34 +0500

    login with server

--- Server/server-Nodejs/Server.js
+++ Server/server-Nodejs/Server.js
@@ -7,12 +7,51 @@ const PORT = 3000;
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// app.get(('/ahmer'), function (req, res, next) {
+//     res.send('hello world!!!');
+// })
 
+let userData = [
+    {
+        email: 'ahmer@gmail.com',
+        pass: '12345'
+    },
+    {
+        email: 'ali@gmail.com',
+        pass: '54321'
+    }
+]
 
-app.get(('/ahmer'), function (req, res, next) {
-    res.send('hello world!!!');
+app.post('/login', (req, res, next) => {
+    const { userEmail, passowrd } = req.body;
+    let isFound = false;
+    console.log(userEmail);
+    console.log(passowrd);
+    if(passowrd.length < 5){
+        return res.send("password length must be at least 5")
+    }
+    for (var i = 0; i < userData.length; i++) {
+        if (userEmail === userData[i].email
+            &&
+            passowrd === userData[i].pass) {
+            isFound = true;
+            return res.send({
+                status: 200,
+                message: 'login successfully'
+            })
+        }
+    }
+
+    if (isFound === false) {
+        res.send({
+                status: 404,
+                message: 'User Not found'
+            })
+    }
+
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
+    // template littrels
     console.log(`server is running on ${PORT}`)
 })
