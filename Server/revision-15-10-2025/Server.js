553cff46762ec46afa2fd16e972d34082213acea
hasanCoder66 <hasanashraf874@gmail.com>
2025-10-15 13:10:31 +0500

    revision stared

---
+++ Server/revision-15-10-2025/Server.js
@@ -0,0 +1,52 @@
+// imports/require from third party or any builin functions
+const express = require("express");
+
+// Describe local and global varriables
+
+// vulnorable way
+// const PORT = 5000 || 3000 || 7000;
+// expacted right soultion to find the available port
+const PORT = 5000 || process.env.PORT;
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// these are the types of API's
+app.post("/createUser", async(req, res) => {
+  // destructure
+  try{
+
+      const { email, name } = req.body;
+      console.log(email);
+      console.log(name);
+      res.send({
+        status: 200,
+        message: "user created successfully",
+      })
+    }catch(err){
+        res.send({
+            err, 
+            status: 500,
+            message: "sorry! server is not responding"
+        })
+    }
+});
+
+// req will be come from frontend (user provided data)
+// res will send to frontend the from server/backend (server responded data/result)
+app.get("/", (req, res) => {
+  res.send("hello world");
+  // console.log("hello world");
+});
+
+// app.put("/api/user",()=>{
+//  const id = req.params 
+// });
+// app.delete();
+
+app.listen(PORT, () => {
+  // template littrels
+  console.log(`Server is running ${PORT}`);
+});
