0f992fee0d6a553fe68a2174e66f7c296ab4a68d
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-11-07 12:52:26 +0500

    user update , decoding token and user data

--- Server/server-Nodejs/public/app.js
+++ Server/server-Nodejs/public/app.js
@@ -8,8 +8,21 @@ async function signup() {
         const lName = document.getElementById('lName').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
+
+        // decding token and fetching user details
+        // const base64Url = token.split('.')[1];
+        // const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Convert Base64Url to standard Base64
+        // const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        //     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        // }).join(''));
         console.log(fName, lName, email, password);
         const res = await axios.post('http://localhost:3000/api/signup', {
+            headers: {
+                'Authorization': `Bearer ${token}`, // For Bearer token authentication
+                'Content-Type': 'application/json' // Or 'application/x-www-form-urlencoded', etc.
+
+            }
+        }, {
             fName,
             lName,
             email,
@@ -31,7 +44,7 @@ async function userDetails() {
         const res = await axios.get('http://localhost:3000/api/userDetails')
         console.log(res)
         let userData = document.getElementById("userdata")
-userData.innerHTML=JSON.stringify(res.data.users)
+        userData.innerHTML = JSON.stringify(res.data.users)
         // .then((res) => {
 
         // })
