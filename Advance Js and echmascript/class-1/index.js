eb6c9ed9248f774cbe875cf1b6a814e8f8303a92
MRMutahir <mutahirkareem820@gmail.com>
2025-11-12 13:08:56 +0500

    es-2020

--- Advance Js and echmascript/class-1/index.js
+++ Advance Js and echmascript/class-1/index.js
@@ -41,10 +41,60 @@
 // console.log(newArr);
 // console.log(sortArray);
 
-const alphabates = ["a", "b", "c", "d"];
+// const alphabates = ["a", "b", "c", "d"];
 
-const resultA = [alphabates]
-const resultB = [...alphabates]
+// const resultA = [alphabates]
+// const resultB = [...alphabates]
 
-console.log(resultA);
-console.log(resultB);
+// console.log(resultA);
+// console.log(resultB);
+
+// console.log(1);
+// // console.log(setTimeout(() => 2, 3000));
+// setTimeout(() => console.log(2), 3000);
+// console.log(3);
+
+// let text = "you are succesfully login";
+// if (text.includes("success")) {
+//   console.log(text);
+// } else {
+//   console.log("not included");
+// }
+
+const myArr = [
+  { name: "X00", price: 100 },
+  //   {name:"X01",price:100 },
+  //   {name:"X02",price:100 },
+  //   {name:"X03",price:100 },
+  { name: "X04", price: 110 },
+  //   {name:"X05",price:110 },
+  //   {name:"X06",price:110 },
+  //   {name:"X07",price:110 },
+  { name: "X08", price: 120 },
+  //   {name:"X09",price:120 },
+  //   {name:"X10",price:120 },
+  { name: "X16", price: 140 },
+  //   {name:"X11",price:120 },
+  { name: "X12", price: 130 },
+  //   {name:"X13",price:130 },
+  //   {name:"X14",price:130 },
+  //   {name:"X15",price:130 },
+  //   {name:"X17",price:140 },
+  //   {name:"X18",price:140 },
+  //   {name:"X19",price:140 }
+];
+console.log(myArr);
+myArr.sort((p1, p2) => {
+  if (p1.price < p2.price) return -1;
+  if (p1.price > p2.price) return 1;
+  return 0;
+});
+console.log(myArr);
+// let txt = "";
+// myArr.forEach(myFunction);
+
+// function myFunction(value) {
+//   txt += value.name + " " + value.price + "<br>";
+// }
+// console.log(txt);
+// document.getElementById("demo").innerHTML = txt;
