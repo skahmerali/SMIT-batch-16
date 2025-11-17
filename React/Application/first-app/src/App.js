5cb078b34a7c4b4462ae3da9b15792ff19c88b09
hasanCoder66 <hasanashraf874@gmail.com>
2025-11-17 12:50:53 +0500

    react inroduction and first class

---
+++ React/Application/first-app/src/App.js
@@ -0,0 +1,35 @@
+// import ahmer from './logo.svg';
+// import './App.css';
+
+function App() {
+  // var ahmer = "ahmer"
+  return (
+    <>
+      <div>
+        <div>
+          Hello world!!!
+        </div>
+
+        {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Learn React
+          </a>
+          </header> */}
+      </div>
+      <div>
+        bye bye world!!!
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ React/guide.md
@@ -0,0 +1,7 @@
+How to start with a new app:
+    first you need to install react in your local pc :
+        npm i create-react-app -g
+    second create your first app:
+        npx create-react-app app-name
+    here we go:
+        you can  see your app is installed successfully and there would be multiples files and folders 
